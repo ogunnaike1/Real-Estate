2fc7a685f387fd9aa51a92cd47dff697f95138e2
ogunnaike1 <ogunnaikeusman17@gmail.com>
2025-03-01 23:53:30 +0100

    commit

--- frontend/src/component/LandingPageCarousel.jsx
+++ frontend/src/component/LandingPageCarousel.jsx
@@ -1,5 +1,6 @@
 import React, { useCallback } from 'react'
-import { motion, spring } from 'framer-motion';
+import { motion, useInView  } from 'framer-motion';
+import { useRef } from "react";
 import Navbar from './Navbar'
 import useEmblaCarousel from "embla-carousel-react";
 import Autoplay from "embla-carousel-autoplay";
@@ -10,11 +11,39 @@ const images = [
   "https://static.vecteezy.com/system/resources/previews/050/785/760/non_2x/beautiful-luxury-house-with-stone-facade-and-landscaped-yard-lighting-up-at-dusk-photo.jpg",
 ];
 
+const parentVariant = {
+    hidden:{
+        X:-150,
+        opacity:0
+    },
+    visible:{
+        x:10,
+        opacity:1,
+        transition:{duration:0.2, mass:0.2, damping:4, delay:0.1, type:"spring", stiffness:120, when:"beforeChildren", staggerChildren:0.2}
+    }
+
+}
+const childrenVariant = {
+    hidden:{
+        x: -50, 
+        opacity:0
+    },
+    visible:{
+        x:0,
+        opacity: 1,
+        transition: { duration: 0.2, type: "spring", stiffness: 180 }
+    
+    }
+
+}
+
 const LandingPageCarousel = () => {
 
   const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true }, [
     Autoplay({ delay: 3000 }), // Auto-play every 4 seconds
   ]);
+  const ref = useRef(null);
+  const isInView = useInView(ref, { margin: "-100px" });
 
 
   return (
@@ -43,21 +72,77 @@ const LandingPageCarousel = () => {
             </div>
 
 
-        </div
+        </div>
     </div>
-    <div className='bg-red-500 h-[200vh] relative top-[100vh]'>
-         <div>
-         <img src="https://media.designcafe.com/wp-content/uploads/2023/08/25160343/living-room-red-couch-with-recliner-lounge.jpg" alt="" />
+    <div className=' h-[91vh] sticky top-0 flex justify-between -z-10'>
+         <div className='w-1/2 h-[100%]'>
+         <img className='h-[100%]' src="https://media.designcafe.com/wp-content/uploads/2023/08/25160343/living-room-red-couch-with-recliner-lounge.jpg" alt="" />
 
          </div>
-         <div>
-         <img src="https://atlanticfinefurniture.com/cdn/shop/articles/Coastal-Design-Ideas-The-Ultimate-Guide-Atlantic-Fine-Furniture-Inc-5958_1200x_a30491ea-7df0-41ae-ac10-0f92c5fbb040_800x.png?v=1731571602" alt="" />
+         <div className='w-1/2 h-[100%]'>
+         <img className='h-[100%]' src="https://atlanticfinefurniture.com/cdn/shop/articles/Coastal-Design-Ideas-The-Ultimate-Guide-Atlantic-Fine-Furniture-Inc-5958_1200x_a30491ea-7df0-41ae-ac10-0f92c5fbb040_800x.png?v=1731571602" alt="" />
 
          </div>
 
     </div>
+    
+    <div className='h-[100vh] flex justify-between '>  {/* find your perfect home container */}
+        <div className='w-[60%] h-[100%] bg-white'>
+            <motion.div ref={ref} variants={parentVariant} initial="hidden"  animate={isInView ? "visible" : "hidden"}   className='flex mx-auto w-[70%] h-[100%] flex-col justify-around text-indigo-950 '>
+                <motion.p variants={childrenVariant} className='text-[40px] font-[600]'>
+                Find Your Perfect Home
+                </motion.p>
+                <motion.p variants={childrenVariant} className='text-[20px] font-[400]'>
+                At Stonebridge, we simplify the rental process by connecting tenants with trusted real estate agents who have the perfect homes for them. Whether you're searching for a modern apartment, a family-friendly house, or a luxury rental, our platform ensures you find a space that truly feels like home—guided by professionals who know the market best.
+                </motion.p>
+                <motion.p variants={childrenVariant}  className='text-[20px] font-[400]'>
+                Finding the right home is more than just browsing listings—it’s about working with the right agent who understands your needs. Our network of experienced real estate agents ensures you get access to properties that align with your lifestyle, location preferences, and budget.
+                </motion.p>
+                <motion.p variants={childrenVariant} className='text-[20px] font-[400]'>
+                Every property on our platform is vetted for accuracy and quality. Agents provide real-time availability and detailed insights, so tenants can rent with confidence, knowing they’re making the right choice. Why waste time searching alone? Let our trusted agents guide you to the perfect rental home, hassle-free.
+                </motion.p>
 
- 
+                <motion.button variants={childrenVariant} whileHover={{scale:1.1}} transition={{repeat:Infinity, type:"tween",duration: 0.5, stiffness:100, repeatType: "reverse"}} className='text-[16px] font-[700] rounded-3xl py-[12px] px-[30px]  border-3 border-indigo-950  text-indigo-950 '>I am interest</motion.button>
+
+            </motion.div>
+        </div>
+        <div className='w-[40%]'>
+
+        </div>
+
+    </div>
+
+    <div className='h-[10vh] bg-white'>
+
+    </div>
+
+
+    <div className='h-[100vh] flex justify-between ' >
+      
+
+        </div>
+        <div className='w-[55%] h-[100%] bg-white'>
+        <motion.div ref={ref} variants={parentVariant} initial="hidden"  animate={isInView ? "visible" : "hidden"}   className='flex mx-auto w-[70%] h-[100%] flex-col justify-around text-indigo-950 '>
+                <motion.p variants={childrenVariant} className='text-[40px] font-[600]'>
+                Spacious & Elegant 3-Bedroom Flat for Rent in Bodija
+                </motion.p>
+                <motion.p variants={childrenVariant} className='text-[20px] font-[400]'>
+                Looking for the perfect home in Bodija? Our three-bedroom flats offer the ideal blend of comfort, convenience, and modern living in one of Ibadan’s most sought-after neighborhoods. Whether you’re a family, a group of professionals, or someone who simply enjoys extra space, this home is designed to meet your needs.
+                </motion.p>
+                <motion.p variants={childrenVariant}  className='text-[20px] font-[400]'>
+                Step into a home that exudes warmth and sophistication. Each three-bedroom flat is designed with large, airy rooms, allowing for natural light to flood in, creating a welcoming and relaxing environment. The master bedroom features an en-suite bathroom, offering a private retreat, while the additional bedrooms are perfect for children, guests, or even a home office.
+                </motion.p>
+                <motion.p variants={childrenVariant} className='text-[20px] font-[400]'>
+                The fully fitted kitchen comes with ample cabinet space, sleek countertops, and high-quality fixtures, making cooking a delight. The open-plan living and dining areas are spacious enough for relaxation and entertainment, whether you’re hosting guests or enjoying a quiet evening at home.
+                </motion.p>
+
+                <motion.button whileHover={{scale:1.1}} transition={{repeat:Infinity, type:"tween",duration: 0.5, stiffness:100, repeatType: "reverse"}} className='text-[16px] font-[700] rounded-3xl py-[12px] px-[30px]  border-3 border-indigo-950  text-indigo-950 '>Meet the Agent</motion.button>
+
+            </motion.div>
+
+        </div>
+    </div>
+
     </div>
 
  
