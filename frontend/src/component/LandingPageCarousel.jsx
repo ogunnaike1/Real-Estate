084dd914486f78c8ff0b2af17036f4fb58be9d54
ogunnaike1 <ogunnaikeusman17@gmail.com>
2025-03-04 23:28:59 +0100

    commit

--- frontend/src/component/LandingPageCarousel.jsx
+++ frontend/src/component/LandingPageCarousel.jsx
@@ -203,41 +203,50 @@ const LandingPageCarousel = () => {
     </div>
 
     <div>
-        <div className=' bg-gray-300'> 
-        <img className='h-[20vh]' src={logo} alt="" />
-        </div>
-
-        <div className='h-[60vh] bg-gray-300 flex justify-between text-[16px]'>
-         
-        
-            <div className='flex gap-[5px] flex-col '>
-                <a className='flex items-center gap-[2px]' href=""><FaAngleRight /> <span>Home</span></a>
-                <a className='flex items-center gap-[2px]' href=""><FaAngleRight /> <span>About Us</span> </a>
-                <a className='flex items-center gap-[2px]' href=""><FaAngleRight /> <span>Listings</span> </a>
-                <a className='flex items-center gap-[2px]' href=""><FaAngleRight /> <span>Gallery</span> </a>
-            </div>
-
-            <div>
-            <a className='flex items-center gap-[2px]' href=""><FaAngleRight /> <span>Contact Us</span> </a>
-            <a className='flex items-center gap-[2px]' href=""><FaAngleRight /> <span>Privacy Policy</span> </a>
-            <a className='flex items-center gap-[2px]' href=""><FaAngleRight /> <span>Terms & Conditions</span> </a>
-            
-            </div>
-
-            <div>
-                
-                <a className='flex items-center gap-[2px]' href=""><IoLocationOutline /> <span>N0 15 okeolugbade street, igbeba, ijebu-ode</span></a>
-                <a className='flex items-center gap-[2px]' href=""><FaWhatsapp /> <span>+234-813-985-65</span></a>
-                <a className='flex items-center gap-[2px]' href=""><CiMail /> <span>Stonebridge@gmail.com</span></a>
-          
-                
-             </div>
-                <div className='flex h-[20vh] bg-amber-300 flex-col gap-[15px]'>
-                <input className='w-[20vw] h-[7vh]  bg-white' type="text"/>
-                <button className='bg-indigo-950 text-white w-[6vw] h-[7vh]'>Sign up</button>
+        <div  className='bg-gray-300 pb-[3vh]'>
+            <div className=' flex mx-auto w-[80%] flex-col'>
+                <div className=''> 
+                <img className='h-[20vh]' src={logo} alt="" />
                 </div>
 
+            
+                
+                
+                    <div className='flex gap-[5px] flex-col '>
+                        <a className='flex items-center gap-[2px]' href=""><FaAngleRight /> <span>Home</span></a>
+                        <a className='flex items-center gap-[2px]' href=""><FaAngleRight /> <span>About Us</span> </a>
+                        <a className='flex items-center gap-[2px]' href=""><FaAngleRight /> <span>Listings</span> </a>
+                        <a className='flex items-center gap-[2px]' href=""><FaAngleRight /> <span>Gallery</span> </a>
+                    </div>
+
+                    <div>
+                    <a className='flex items-center gap-[2px]' href=""><FaAngleRight /> <span>Contact Us</span> </a>
+                    <a className='flex items-center gap-[2px]' href=""><FaAngleRight /> <span>Privacy Policy</span> </a>
+                    <a className='flex items-center gap-[2px]' href=""><FaAngleRight /> <span>Terms & Conditions</span> </a>
+                    
+                    </div>
+
+                    <div className=''>
+                        <span className='font-[700]'>Contact Us</span>
+                        <a className='flex items-center gap-[2px]' href=""><span><IoLocationOutline /></span> <span >N0 15 okeolugbade street, igbeba, ijebu-ode</span></a>
+                        <a className='flex items-center gap-[2px]' href=""><FaWhatsapp /> <span>+234-813-985-65</span></a>
+                        <a className='flex items-center gap-[2px]' href=""><CiMail /> <span>Stonebridge@gmail.com</span></a>
+                
+                        
+                    </div>
+                       
+                        <div className='flex h-[20vh] flex-col gap-[10px]'>
+                        <span className='font-[700]'>Remain Updated</span>
+                        <input className='w-[20vw] h-[8vh]  bg-white' type="text" placeholder='Your Email Address'/>
+                        <button className='bg-indigo-950 text-white w-[6vw] h-[7vh]'>Sign up</button>
+                        </div>
+
+                </div>
+            </div>
+            <div className='text-[14px] text-center'>© 2025 All Rights Reserved </div>
         </div>
+        
+       
     </div>
 
     </div>
