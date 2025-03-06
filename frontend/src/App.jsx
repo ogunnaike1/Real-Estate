b0da462abad0a015f21c409ab6f6db10768adfc2
ogunnaike1 <ogunnaikeusman17@gmail.com>
2025-03-06 23:56:09 +0100

    commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,21 @@
 import React from 'react'
 import reactLogo from './assets/react.svg'
+import { Routes, Route } from "react-router-dom";
 import viteLogo from '/vite.svg'
 import './App.css'
 import LandingPageCarousel from './component/LandingPageCarousel'
+import LoginPage from './component/LoginPage';
 
 function App() {
 
-
   return (
-    <>
-    <LandingPageCarousel/>
-    </>
+
+    <Routes>
+
+    <Route path="/landing-page" element= {<LandingPageCarousel/>} />
+    <Route path="/login" element={<LoginPage />} />
+  </Routes>
+
   )
 }
 
---
+++ frontend/src/component/LoginPage.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { IoClose } from "react-icons/io5";
+
+const LoginPage = () => {
+  return (
+    <div className='w-full bg-red-400 h-screen flex justify-center items-center '>
+
+    <div className='bg-white h-[80vh] w-[27vw] flex rounded-2xl'>
+     <div className='flex items-center justify-between h-[5vh] w-full bg-red-200'>
+        <h1>Welcome to Stonebridge</h1>
+      
+     </div>
+        <div>
+
+        </div>
+    </div>
+
+    </div>
+  )
+}
+
+export default LoginPage
