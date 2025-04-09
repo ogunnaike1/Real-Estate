debc8bc804fcb6b30f62a67be91bcd70771aa970
ogunnaike1 <ogunnaikeusman17@gmail.com>
2025-04-09 23:37:49 +0100

    commit

--- frontend/src/component/AgentPage.jsx
+++ frontend/src/component/AgentPage.jsx
@@ -2,17 +2,46 @@ import React from 'react'
 
 const AgentPage = () => {
   return (
-    <div className='h-screen w-full bg-blue-300 absolute top-0'>
-      <div className='h-screen relative top-0 w-full'>
-      <img className='h-[100%] w-[100%]' src="https://en.idei.club/uploads/posts/2023-12/1703105533_en-idei-club-p-beautiful-house-inside-and-outside-dizain-16.jpg" alt="" />
-      </div>
+    <>
+      <div className='relative top-[-9vh] font-sans w-full'>
+            <div className='h-screen text-8xl relative top-0 w-full'>
+          <div className='h-screen relative top-0 w-full'>
+            <img className='h-[100%] w-[100%]' src="https://en.idei.club/uploads/posts/2023-12/1703105533_en-idei-club-p-beautiful-house-inside-and-outside-dizain-16.jpg" alt="" />
+            </div>
+           
+          </div>
 
-      <div className=' absolute top-[50px] h-[2000px] bg-amber-500 '>
+          <div className='h-[5000px] bg-white relative mt-[-50vh]'>
 
+            <div className='w-[80%] mx-auto h-[100vh] bg-white'>
+              <div className=''>
+                <div className='flex gap-[20px] h-[15vh] border-b-[1px]  border-slate-200'>
+                  <div className='h-[90px] w-[90px] mt-[-25px]'>
+                    <img className='h-[100%] rounded-[50%]' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_VkrGsURfsUriJFM6bAD51TjXzrcEYo2RAQ&s" alt="" />
+                  </div>
+                  <div className='mt-[-10px]'>
+                     <div className='flex gap-[20px]'>
+                      <div className='bg-indigo-900 px-[20px] py-[3px] rounded-2xl text-[14px] text-white'>
+                        Agent
+                      </div>
+                       <div className='h-[22px]'>
+                        <img className='h-[100%] w-[30px]' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhRDlTt0IeFQReH-xeVsEytDGNdQ6_87yk4A&s" alt="" />
+                       </div>
 
-      </div>
-   
+                     </div>
+                     <div className='font-[500] text-[20px]'> Stonebridge</div>
+                     <div className='text-green-200'> Active </div>
+                     <div className='text-[12px] font-[300]'>joined 2020 june 12</div>
+                  </div>
+                  
+                </div>
+              </div>
+
+            </div>
+
+          </div>
     </div>
+    </>
   )
 }
 
