3cd5d884696aa4852a2457eea620f63a8dd55501
ogunnaike1 <ogunnaikeusman17@gmail.com>
2025-04-11 23:53:39 +0100

    commit

--- frontend/src/component/AgentPage.jsx
+++ frontend/src/component/AgentPage.jsx
@@ -60,7 +60,8 @@ const AgentPage = () => {
                        </div>
 
                        <div className='flex flex-col'>
-                 
+                          <span>Stonebridge</span>
+                          <span>Available</span>
                        </div>
                      </div>
 
