4d3887b8785ce4e7287902c902eecf8b79c0fb2c
ogunnaike1 <ogunnaikeusman17@gmail.com>
2025-05-01 23:31:58 +0100

    commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -33,7 +33,7 @@ const App= ()=> {
   }
     <Routes>
     <Route path="/"element= {<LandingPageCarousel/>} />
-
+    <Route path="/agent" element={<AgentPage />} />
   </Routes>
   <Footer/>
   </div>
